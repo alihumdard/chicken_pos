d7b80f1040dff8025e6b627964e0505ca8578fdd
ali <ahmadshahzadrajpoot005@gmail.com>
2025-12-25 14:33:55 +0500

    push

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
     {{-- <meta name="viewport" content="width=device-width, initial-scale=1, "> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title' , 'Urban Media')</title>
+    <title>@yield('title' , 'Chicken Pos')</title>
     
     <style>
         /* 🟢 FIX: Scroll wapis lane ke liye overflow: auto kar diya */
